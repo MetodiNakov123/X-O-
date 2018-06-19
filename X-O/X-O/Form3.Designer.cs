02bfa17fa121d3f2893eda3e9380600cdb287b45
dpaunov21 <daunov21@gmail.com>
2018-06-19 19:15:02 +0200

    Final

---
+++ X-O/X-O/Form3.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace X_O {
+    partial class Form3 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnSingleplayer = new System.Windows.Forms.Button();
+            this.btnMultiplayer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblTitle.Location = new System.Drawing.Point(114, 45);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(62, 31);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "X-O";
+            // 
+            // btnSingleplayer
+            // 
+            this.btnSingleplayer.Location = new System.Drawing.Point(12, 109);
+            this.btnSingleplayer.MaximumSize = new System.Drawing.Size(117, 50);
+            this.btnSingleplayer.MinimumSize = new System.Drawing.Size(117, 50);
+            this.btnSingleplayer.Name = "btnSingleplayer";
+            this.btnSingleplayer.Size = new System.Drawing.Size(117, 50);
+            this.btnSingleplayer.TabIndex = 1;
+            this.btnSingleplayer.Text = "Компјутер";
+            this.btnSingleplayer.UseVisualStyleBackColor = true;
+            this.btnSingleplayer.Click += new System.EventHandler(this.btnSingleplayer_Click);
+            // 
+            // btnMultiplayer
+            // 
+            this.btnMultiplayer.Location = new System.Drawing.Point(153, 109);
+            this.btnMultiplayer.Name = "btnMultiplayer";
+            this.btnMultiplayer.Size = new System.Drawing.Size(117, 50);
+            this.btnMultiplayer.TabIndex = 2;
+            this.btnMultiplayer.Text = "Втор Играч";
+            this.btnMultiplayer.UseVisualStyleBackColor = true;
+            this.btnMultiplayer.Click += new System.EventHandler(this.btnMultiplayer_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 185);
+            this.Controls.Add(this.btnMultiplayer);
+            this.Controls.Add(this.btnSingleplayer);
+            this.Controls.Add(this.lblTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Нова Игра";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnSingleplayer;
+        private System.Windows.Forms.Button btnMultiplayer;
+    }
+}
---
+++ X-O/X-O/Form3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_O {
+    public partial class Form3 : Form {
+        public Form3() {
+            InitializeComponent();
+        }
+
+        private void btnSingleplayer_Click(object sender, EventArgs e) {
+
+            DialogResult = DialogResult.OK;
+        }
+
+        private void btnMultiplayer_Click(object sender, EventArgs e) {
+            DialogResult = DialogResult.Abort;
+        }
+    }
+}
